03784c363c40c2fd43aa226fa8d04575c830780a
Alex <alex@Alexs-MacBook-Air.local>
2021-04-23 16:30:04 -0700

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-
-    this.state = { monsters: [
-      { name: 'Frankenstein', id: 'asc1'},
-      { name: 'George', id: 'asc2'},
-      { name: 'Banshee', id: 'asc3'},
-      { name: 'bigMonster', id: 'asc4'},
-      { name: 'littleMonster', id: 'asc5'}
-    ]
+    this.state = { monsters: [] }
   }
+  componentDidMount() {
+    fetch('https://pokeapi.co/api/v2/pokemon').then(response => response.json())
+    .then(users => this.setState({monsters: users.results}))
   }
   render() {
     return(
