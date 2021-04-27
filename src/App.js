b99c691919d63aece320e18b71b4dbaf5fd79c23
Alex <alex@Alexs-MacBook-Air.local>
2021-04-27 13:10:24 -0700

    searchbar filters monsters by name

--- src/App.js
+++ src/App.js
@@ -15,12 +15,16 @@ class App extends Component {
     .then(users => this.setState({monsters: users}))
   }
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
     return(
       <div className = 'App'>
         <input type='search' placeholder='search monsters' onChange={e => this.setState( {searchField: e.target.value}, () => 
         console.log(this.state) )
         }/>
-        <CardList monsters={this.state.monsters}>
+        <CardList monsters={filteredMonsters}>
 
         </CardList>
       </div>
