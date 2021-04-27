cc3f67307daff81e8f10be69c60bf96adf412148
Alex <alex@Alexs-MacBook-Air.local>
2021-04-26 17:45:16 -0700

    Add onchange event to searchbar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-    this.state = { monsters: [] }
+    this.state = { 
+      monsters: [],
+      searchField: ''
+    }
   }
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
@@ -14,6 +17,9 @@ class App extends Component {
   render() {
     return(
       <div className = 'App'>
+        <input type='search' placeholder='search monsters' onChange={e => this.setState( {searchField: e.target.value}, () => 
+        console.log(this.state) )
+        }/>
         <CardList monsters={this.state.monsters}>
 
         </CardList>
